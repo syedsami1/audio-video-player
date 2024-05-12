557854684d03a562d6b659158218062b0f02f8c0
syedsami1 <syedsami40525@gmail.com>
2024-05-12 21:48:02 +0530

    Added new feature for media playback

--- src/components/RegularVideo.jsx
+++ src/components/RegularVideo.jsx
@@ -1,15 +1,28 @@
+import React from 'react';
 import ReactPlayer from 'react-player';
-function RegularVideo() {
-	return (
-		<div className='video-wrapper'>
-			<ReactPlayer
-				url='https://www.youtube.com/watch?v=8kpDC2I_8n0'
-				playing
-				loop
-				className='react-player-regular'
-			/>
-		</div>
-	);
+
+function MediaCarousel() {
+  const mediaURLs = [
+    'https://www.youtube.com/watch?v=8kpDC2I_8n0',
+    'https://youtu.be/ZQ3G35sgqso?si=-VyYQlRzpJYDU4Ut',
+    'https://youtu.be/DEhUQHXbmI8?si=SNtdT_f4VWj47o0Z',
+    'https://youtu.be/xRnVxAZ1F8k?si=xUEBtd497p9eRJHw',
+    'https://youtu.be/otlGMjNSJNw?si=oxM6hufvGq4SYJVa'
+  ];
+
+  return (
+    <div className='media-carousel'>
+      {mediaURLs.map((url, index) => (
+        <ReactPlayer
+          key={index}
+          url={url}
+          playing
+          loop
+          className='react-player-carousel'
+        />
+      ))}
+    </div>
+  );
 }
 
-export default RegularVideo;
+export default MediaCarousel;
